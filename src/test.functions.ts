5817a632e1a5260909bf20b91cd5fea978cb34ab
fboeller <fabianboeller@googlemail.com>
2022-02-10 12:05:57 -0600

    Fix openapi, add validation to tests

--- src/test.functions.ts
+++ src/test.functions.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
+import { validateInputs } from "./pre-request-handlers/openapi";
 
 export function server(configure: (express: Express) => void): Express {
   const app = express();
   app.use(express.json({ limit: "1mb" }));
+  app.use(validateInputs);
   configure(app);
   return app;
 }
